9f5ed4239f47117b4151e27c70084d771e8881b3
Felipe dos Santos <felipemirandajd13@hotmail.com>
2020-07-27 11:08:17 -0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const nunjucks = require('nunjucks');
+const { static } = require('express');
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res) {
+    const about = {
+        avatar_url: "assets/img/frank.png",
+        name: "Francisco Ferreira",
+        role: "Professor de Matemática",
+        description: "Sempre tentando fazer o melhor para isso e aquilo, um texto aiLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec  maximus.",
+    }
+
+
+    return res.render("about", { about })
+})
+
+server.get("/classes", function(req, res) {
+
+    return res.render("classes", {items: videos })
+})
+
+server.get("/video", function(req, res) {
+    const id = req.query.id
+
+    const video = videos.find(function(video){
+        return video.id == id
+    })
+
+    if (!video) {
+        return res.send("Video not found")
+    }
+
+    return res.render("video", {item: video})
+
+
+})
+
+server.listen(5000, function() {
+    console.log("server is running")
+})
