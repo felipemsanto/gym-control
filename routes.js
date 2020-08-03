82e6c9a5838c1876090808b4d147a05a0cbd7ec0
Felipe dos Santos <felipemirandajd13@hotmail.com>
2020-08-03 11:01:47 -0300

    Routes steap 7 and create page start

--- routes.js
+++ routes.js
@@ -11,6 +11,14 @@ routes.get('/instructors', function(req, res) {
 
 })
 
+routes.get('/instructors/create', function(req, res) {
+    return res.render("instructors/create")
+})
+
+routes.post("/instructors", function(req, res){
+    return res.send("recebido")
+})
+
 routes.get('/members', function(req, res) {
     return res.send("members")
 
